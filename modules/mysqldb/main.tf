5f202e0f69148e40dadb48e293cbb2ee1289b4cf
satyam pandey <satyampandey@192.168.1.4>
2025-09-21 12:11:17 +0530

    hello

--- modules/mysqldb/main.tf
+++ modules/mysqldb/main.tf
@@ -10,7 +10,7 @@ resource "aws_db_instance" "this" {
   engine                  = "mysql"
   engine_version          = var.engine_version
   instance_class          = var.instance_class
-  name                    = var.db_name
+  db_name                 = var.db_name
   username                = var.db_username
   password                = var.db_password
   db_subnet_group_name    = aws_db_subnet_group.this.name
